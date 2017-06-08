ced9189aa7107caaa0f713522cbce82e3ce60b05
Kate <petryaeva.e@gmail.com>
2017-06-08 10:45:08 +0300

    добавлены tablet и desktop версии, подключила шрифт PTSansNarrow

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,27 +1,52 @@
 (function($) {
-  
- //  $('.slider--full-screen').slick({
-	// 	slidesToShow: 1,
-	// 	slidesToScroll: 1,
-	// 	arrows: true,
-	// 	dots: true,
-	// 	autoplay: true,
-	// 	infinite: true,
-	// 	prevArrow: '<button type="button" class="slider__arrow--prev"></button>',
-	// 	nextArrow: '<button type="button" class="slider__arrow--next"></button>',
-	//     responsive: [
-	//   	{
-	//   		breakpoint: 1024,
-	//   		settings: {
-	//   			arrows: false
-	//   		}
-	//   	}
-	//   	]
-	// });
 
-  // popup для видео в слайдере
+	// .videos слайдер с видео
 
-  $(".videos__item").fancybox({
+	$('.video-list .videos').slick({
+		mobileFirst: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		pauseOnHover: true,		
+		slidesToShow: 1,
+		autoplay: true,
+		arrows: false,
+		dots: false,	
+		rows: 3,
+		responsive: [			
+		  	{
+		  		breakpoint: 768,
+		  		settings: {
+		  			slidesToShow: 2,
+					slidesToScroll: 2,
+					arrows: true,
+					dots: false,
+					rows: 1,
+					autoplay: true,
+					infinite: true,
+					prevArrow: '<button type="button" class="slick-arrow--prev"></button>',
+					nextArrow: '<button type="button" class="slick-arrow--next"></button>',
+		  		}
+		  	},
+		  	{
+		  		breakpoint: 968,
+		  		settings: {
+		  			slidesToShow: 3,
+					slidesToScroll: 3,
+					arrows: true,
+					dots: false,
+					rows: 1,
+					autoplay: true,
+					infinite: true,
+					prevArrow: '<button type="button" class="slick-arrow--prev"></button>',
+					nextArrow: '<button type="button" class="slick-arrow--next"></button>',
+		  		}
+		  	}
+		 ]
+	});
+
+  	// popup для видео в слайдере
+
+  	$(".videos__item").fancybox({
 		'padding'		: 0,
 		'autoScale'		: false,
 		'transitionIn'	: 'none',
@@ -36,11 +61,11 @@
 		}
 	});
   
-  // btn--menu показать главное меню на мобильном 
+  	// btn--menu показать главное меню на мобильном 
 
-  $('.btn--menu').click(function () {
-    $('.header__menu').toggleClass('header__menu--opened');
-    $('.btn--menu').toggleClass('btn--menu-opened');
-  });
+  	$('.btn--menu').click(function () {
+    	$('.header__menu').toggleClass('header__menu--opened');
+    	$('.btn--menu').toggleClass('btn--menu-opened');
+  	});
 
 }(jQuery));
